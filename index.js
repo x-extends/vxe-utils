84fb21db200faa11fce3b8e25cccc384cb8156ab
Xu Liangzhan <xu_liangzhan@163.com>
2018-04-28 22:49:06 +0800

    1.3.9

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 function VXEUtils (Vue, XEUtils, options) {
-  Object.defineProperty(Vue.prototype, '$utils', {
-    get: function () {
-      XEUtils.$context = this
-      return XEUtils
-    }
-  })
-  if (options && options.mounts && options.mounts.length) {
-    var mounts = options.mounts.join(';')
-    if (mounts.indexOf('locat') > -1) {
-      Object.defineProperty(Vue.prototype, '$locat', {
-        get: XEUtils.locat
-      })
-    }
-    if (mounts.indexOf('browse') > -1) {
-      Object.defineProperty(Vue.prototype, '$browse', {
-        get: XEUtils.browse
-      })
-    }
-    if (mounts.indexOf('cookie') > -1) {
-      Object.defineProperty(Vue.prototype, '$cookie', {
-        get: function () {
-          return XEUtils.cookie
-        }
-      })
+  var mounts = options && options.mounts && options.mounts.length ? options.mounts.join(';') : []
+  var definePro = function (name, getFn) {
+    Object.defineProperty(Vue.prototype, '$' + name, {get: getFn})
+  }
+  var setMount = function (name, callback) {
+    if (mounts.indexOf(name) > -1) {
+      definePro(name, callback || XEUtils[name])
     }
   }
+  definePro('utils', function () {
+    XEUtils.$context = this
+    return XEUtils
+  })
+  setMount('cookie', function () {
+    return XEUtils.cookie
+  })
+  setMount('browse')
+  setMount('locat')
 }
 
 module.exports = VXEUtils
